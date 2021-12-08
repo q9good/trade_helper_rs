4e098984f19e70e6ede64f588e6ef48cfb3b537b
q9good <psnyima@gmail.com>
2021-12-08 22:35:30 +0800

    parse cmd

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,38 @@ mod event;
 mod market;
 mod strategy;
 use time::{macros::*, OffsetDateTime};
+use clap::Parser;
 #[allow(clippy::zero_prefixed_literal)]
 // use chrono::{Datelike, Local, };
 // use crossbeam_channel::{bounded, unbounded};
 
+/// A CLI APP with fund AIP
+#[derive(Parser, Debug)]
+#[clap(author, about, version)]
+#[clap(name = "FUND AIP")]
+struct Opt {
+    /// the first day begin to buy fund
+    #[clap(short, long, required = true)]
+    start: u32,
+
+    /// the last day stop to buy fund
+    #[clap(short, long, required = true)]
+    end: u32,
+
+    /// the nth day buying fund in a month, default to first day
+    #[clap(short, long, default_value = "1")]
+    day: u8,
+
+    /// the list of fund code
+    #[clap(name = "LIST OF FUND", short = 'f', long, required = true, min_values = 1)]
+    fund:Vec<u32>,
+
+    /// the buying amount of each fund
+    #[clap(name = "BUDGET OF FUND", short = 'b', long, required = true, min_values = 1)]
+    budget:Vec<f32>
+}
+
 fn main() {
-    let funds = vec![002021u32, 005343];
-    let _start_date = date!(2007 - 1 - 1);
-    let _today = OffsetDateTime::now_local().unwrap().date();
+    let opt = Opt::parse();
+    println!("{:?}",opt);
 }
